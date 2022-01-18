e71ea89859bdbc18b0fd960d330f283b05c75f43
youbyoub48 <ayou8@hotmail.fr>
2022-01-18 20:45:27 +0100

    bouton

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function Bouton(){
-    for (let i=0; i<9; i++){
-        // crée un nouvel élément div
-    var newDiv = document.createElement("button");
-    // et lui donne un peu de contenu
-    var newContent = document.createTextNode('NSI');
-    // ajoute le nœud texte au nouveau div créé
-    newDiv.appendChild(newContent);
-
-    // ajoute le nouvel élément créé et son contenu dans le DOM
-    var currentDiv = document.getElementById('div1');
-    document.body.insertBefore(newDiv, currentDiv);
+    for (const element in data) {
+    // crée un nouveau bouton
+    var newButton = document.createElement("button");
+    // texte du bouton
+    var newTexte = document.createTextNode(element);
+    // ajoute le nœud texte au nouveau bouton créé
+    newButton.appendChild(newTexte);
+    // on lui donne la class Bouton
+    newButton.className = "Bouton"
+    newButton.onclick = Test
+    // ajoute le bouton au body du html
+    document.body.appendChild(newButton)
     }
 }
 
-function readTextFile(file, callback) {
+function readTextFile(file, callback) { // cette fonction sert a récuperer les données d'un fichier json
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
@@ -27,8 +28,21 @@ function readTextFile(file, callback) {
 
 
 function Lecture(){
-    readTextFile("./data.json", function(text){
-        var data = JSON.parse(text);
-        console.log(data);
+    readTextFile("./data.json", function(text){ //on appelle la  fonction readTextFile pour récupérer le contenu de data.json
+        data = JSON.parse(text); // on mets le contenue de data.json dans la variable data
+        console.log(data); // on print la variable data pour voir si tout marche bien
+        Liste_Spe()
+        Bouton()
     });
+}
+
+function Liste_Spe(){
+    for (const element in data) {
+        console.log(element)
+    }
+}
+
+
+function Test() {
+    console.log("marche")
 }
